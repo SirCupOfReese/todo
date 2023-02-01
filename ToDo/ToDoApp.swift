06d85a74efe8b0529b5c7b01982a2e84f5dc36cd
Sloan Phillippi <sphillippi@Sloans-MBP.lan1>
2023-02-01 15:23:42 -0500

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Sloan Phillippi on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
